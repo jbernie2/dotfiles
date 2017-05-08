b3e2c70e68d192faf7430d3d2e173e6d88c542eb
John Bernier <john@John-Berniers-MacBook.local>
2017-05-07 22:10:04 -0400

    Ask user whether they want to overwrite exisiting files

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ install_dir=$HOME
 
 mkdir $install_dir/
 mkdir $install_dir/.bash
-cp -vR .bash/ $install_dir/.bash
-cp -v .bash_profile $install_dir
-cp -v .tmux.conf $install_dir
+cp -viR .bash/ $install_dir/.bash
+cp -vi .bash_profile $install_dir
+cp -vi .tmux.conf $install_dir
