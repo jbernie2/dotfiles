c1eed8c8928e400f65edef5e410813635329dd6d
John Bernier <john@John-Berniers-MacBook.local>
2017-05-30 23:21:10 -0400

    Add Vundle installation, .vimrc, and vim plugin installation to install

--- install.sh
+++ install.sh
@@ -8,3 +8,17 @@ cp -viR .bash/ $install_dir/.bash
 cp -vi .bash_profile $install_dir
 cp -vi .tmux.conf $install_dir
 cp -vi .gitconfig $install_dir
+
+# VIM CONFIG
+
+#install vundle if not installed
+vundle_script="${HOME}/.vim/bundle/Vundle.vim"
+if [ ! -d $vundle_script ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+#copy over vimrc
+cp -vi .vimrc $install_dir
+
+#install vim plugins
+vim +PluginInstall +qall
