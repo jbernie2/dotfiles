2f015266d7acdb68933fcb7e4266fb69a1e7390e
John Bernier <john.bernier@stash.com>
2021-11-07 14:35:03 -0500

    Source all configs copied from configs/bash on shell start

--- configs/bash/source.sh
+++ configs/bash/source.sh
@@ -1,12 +1,8 @@
-echo ">> bash source: $BASH_SOURCE"
-echo ">> bash config home: $BASH_CONFIG_HOME"
 this_file=$(basename $BASH_SOURCE)
 
-for file in $BASH_CONFIG_HOME; do 
+for file in "$BASH_CONFIG_HOME"/*; do
     if [ -f "$file" ]; then 
-        echo ">> comparing $file to $this_file"
-        if [ ! $file == $this_file ]; then
-          #echo "sourcing file: $file"
+        if [ ! $(basename $file) == $this_file ]; then
           source $file
         fi
     fi 
--- scripts/source_config_on_startup.sh
+++ scripts/source_config_on_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bash_config_home=$1
-set_bash_home_command="BASH_CONFIG_HOME=$bash_config_home"
+set_bash_home_command="export BASH_CONFIG_HOME=$bash_config_home"
 source_command='source "$HOME/.bash/source.sh"'
 bash_profile=~/.bash_profile
 
