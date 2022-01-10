367adabe048dd03bda48732edabc2d825642f563
John Bernier <john.bernier@stash.com>
2022-01-09 20:01:40 -0500

    Allow functions defined in configs/bash/functions to be called from command line

--- scripts/bash/source_config_on_startup.sh
+++ scripts/bash/source_config_on_startup.sh
@@ -2,6 +2,7 @@
 bash_config_home=$1
 set_bash_home_command="export BASH_CONFIG_HOME=$bash_config_home"
 source_command='source "$HOME/.bash/source.sh"'
+add_functions_command='PATH=$BASH_CONFIG_HOME/functions:$PATH'
 bash_profile=~/.bash_profile
 
 if grep -x "$set_bash_home_command" ~/.bash_profile
@@ -18,3 +19,9 @@ else
   echo $source_command >> $bash_profile
 fi
 
+if grep -x "$add_functions_command" ~/.bash_profile
+then
+    : # no op
+  else
+      echo $add_functions_command >> $bash_profile
+fi
