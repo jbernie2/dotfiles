27ead296b30fa9c48f0d6cd610ec0e7736cdfed0
John Bernier <john.bernier@stash.com>
2022-01-26 10:01:20 -0500

    Use ripgrep as default search command for fzf

--- configs/bash/fzf.sh
+++ configs/bash/fzf.sh
@@ -1,2 +1,7 @@
-export FZF_DEFAULT_COMMAND="find ."
 export FZF_CTRL_T_COMMAND="find ."
+
+if type rg &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden'
+else
+  export FZF_DEFAULT_COMMAND="find ."
+fi
