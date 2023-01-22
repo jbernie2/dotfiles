4e1f190bbe89fe62094d3aeb50a4023fce721909
John Bernier <john.bernier@stash.com>
2023-01-22 15:33:11 -0500

    Don't shift '-' into '_' when using CAPS_WORD

--- keyboards/ergodox_ez/src/keymap.c
+++ keyboards/ergodox_ez/src/keymap.c
@@ -250,3 +250,23 @@ bool get_combo_must_press_in_order(uint16_t combo_index, combo_t *combo) {
             return true;
     }
 }
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_MINS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
