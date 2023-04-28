466cbacfa6187df1356503c2d4d7e8311e814525
John Bernier <john.bernier@stash.com>
2023-04-28 15:45:29 -0400

    Make Ctags aware of gitignore file

--- configs/ctags/.ctags.d/generate_ctags.sh
+++ configs/ctags/.ctags.d/generate_ctags.sh
@@ -4,5 +4,14 @@ in_git_repo=$(
     echo "false"
 )
 if [ $in_git_repo = "true" ]; then
-  ctags -R
+  # include paths from the repo's git ignore
+  paths_to_ignore=$(git check-ignore *)
+  ignore_file=mktemp
+  echo $paths_to_ignore > $ignore_file
+
+  # pass file to ctags to ignore files specified by repo
+  # in addition to files ignored by ctags config
+  ctags --exclude=@$ignore_file -R
+
+  rm $ignore_file
 fi
